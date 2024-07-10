14cced3ccd202453f8e83beca1d514a787d9322b
Samar Abbas <samarabbas172003@gmail.com>
2024-07-10 21:27:55 +0530

    Mutation

--- server/app.js
+++ server/app.js
@@ -17,6 +17,30 @@ const resolvers = {
     review(_, args) {
       return db.reviews.find((review) => review.id === args.id);
     },
+    Game: {
+      reviews(parent) {
+        return db.games.filter((r) => r.game_id === parent.id);
+      },
+    },
+    Author: {
+      reviews(parent) {
+        return db.reviews.filter((r) => r.author_id === parent.id);
+      },
+    },
+    Review: {
+      author(parent) {
+        return db.authors.find((a) => a.id === parent.author_id);
+      },
+      game(parent) {
+        return db.games.find((g) => g.id === parent.game_id);
+      },
+    },
+  },
+  Mutation: {
+    deleteGame(_, args) {
+      db.games = db.games.filter((g) => g.id !== args.id);
+      return db.games;
+    },
   },
 };
 //Server Setup
--- server/schema.js
+++ server/schema.js
@@ -9,18 +9,29 @@ type Review{
     id:ID!
     rating:Int!
     content:String!
+    game:Game!
+    author:Author!
+
 }
     type Author{
     id:ID!
     name:String!
     verified:Boolean!
+    reviews:[Review!]
     }
+
     type Query{
     reviews:[Review]
     review(id:ID!):Review
     games:[Game]
+    game(id:ID!):Game
     authors:[Author]
-    
+    author(id:ID!):Author
+    }
+
+    type Mutation{
+    deleteGame(id:ID!):[Game]
+
     }
 
 
